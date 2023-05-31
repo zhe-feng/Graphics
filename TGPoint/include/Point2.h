6dd3d470ef3f9902d2b1340c0ee41686ab8910bf
国庆 谭 <295275347@qq.com>
2023-05-31 08:35:42 +0800

    yuan

---
+++ TGPoint/include/Point2.h
@@ -0,0 +1,22 @@
+class CPoint2
+{
+public:
+	CPoint2(void);
+	virtual ~CPoint2(void);
+	CPoint2(double x, double y);
+	//CPoint2(double x, double y, CRGB c);
+	friend CPoint2 operator + (const CPoint2 &p0, const CPoint2 &p1);//运算符重载
+	friend CPoint2 operator - (const CPoint2 &p0, const CPoint2 &p1);
+	friend CPoint2 operator * (const CPoint2 &p, double scalar);
+	//friend CP2 operator * (double scalar, const CP2 &p);
+	//friend CP2 operator / (const CP2 &p, double scalar);
+	//friend CP2 operator += (CP2 &p0, CP2 &p1);
+	//friend CP2 operator -= (CP2 &p0, CP2 &p1);
+	//friend CP2 operator *= (CP2 &p, double scalar);
+	//friend CP2 operator /= (CP2 &p, double scalar);
+public:
+	double x;
+	double y;
+	double w;
+	//CRGB  c;
+};
